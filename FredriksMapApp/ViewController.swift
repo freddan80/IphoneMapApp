407fbc3be018d88ad23f0d0af31d579d51c10d26
Girls Only <GirlsOnly@GirlsiMac.nc.rr.com>
2015-12-13 10:15:58 -0500

    Added Hunnebo as startup point.

--- FredriksMapApp/ViewController.swift
+++ FredriksMapApp/ViewController.swift
@@ -7,18 +7,31 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // set initial location in Hunnebo
+        let initialLocation = CLLocation(latitude: 58.4405, longitude: 11.3174)
+        centerMapOnLocation(initialLocation)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+            regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
+    //override func didReceiveMemoryWarning() {
+    //    super.didReceiveMemoryWarning()
+    //    // Dispose of any resources that can be recreated.
+    //}
 
 
 }
